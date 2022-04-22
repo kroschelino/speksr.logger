4cc8e3c78c9fab609fa21cf60701a1612ea3ce7b
Stefan Kroschel <s.kroschel@slashwhy.de>
2022-04-22 23:51:40 +0200

    Initial commit

---
+++ speksr.testlogger/SpeksrLogger.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+namespace Microsoft.VisualStudio.TestPlatform.Extensions.SpeksrLogger;
+
+
+[FriendlyName(FriendlyName)]
+[ExtensionUri(ExtensionUri)]
+public class SpeksrLogger : ITestLogger
+{
+    /// <summary>
+    /// Uri used to uniquely identify the logger.
+    /// </summary>
+    public const string ExtensionUri = "logger://Microsoft/TestPlatform/XUnitXmlLogger/v1";
+
+    /// <summary>
+    /// Alternate user friendly string to uniquely identify the console logger.
+    /// </summary>
+    public const string FriendlyName = "xunit";
+
+    /// <summary>
+    /// Initializes the Test Logger.
+    /// </summary>
+    /// <param name="events">Events that can be registered for.</param>
+    /// <param name="testRunDirectory">Test Run Directory</param>
+    public void Initialize(TestLoggerEvents events, string testRunDirectory)
+    {
+
+    }
+
+}
